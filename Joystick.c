51d86f84c2e450157da68b8cc651ee622181c759
fullblue <65227601+Chiuchiyin@users.noreply.github.com>
2023-07-21 14:54:33 +0800

    first round only (for testing)

--- Joystick.c
+++ Joystick.c
@@ -42,7 +42,8 @@ typedef enum {
 	CAMERA_LEFT,
 	CAMERA_RIGHT,
 	ZL,
-	ZR
+	ZR,
+	CAPTURE
 } Buttons_t;
 
 typedef struct {
@@ -60,139 +61,122 @@ static const command step[] = {
 	{ A,          5 },
 	{ NOTHING,  250 },
 
-	// Talk to Pondo
+	// Talk to The Academy Ace Tournament guy
 	{ A,          5 }, // Start
 	{ NOTHING,   30 },
-	{ B,          5 }, // Quick output of text
-	{ NOTHING,   20 }, // Halloo, kiddums!
-	{ A,          5 }, // <- I'll try it!
+	{ A,          5 }, // Hello there! Would you like to enter ....
+	{ NOTHING,   15 }, // 
+	{ A,          5 }, // <- Yes
 	{ NOTHING,   15 },
-	{ B,          5 },
+	{ A,          5 }, // .... , a champion-ranked trainer
 	{ NOTHING,   20 },
-	{ A,          5 }, // <- OK!
+	{ A,          5 }, // OK, you're all registered as a contestant. Please head to...
 	{ NOTHING,   15 },
-	{ B,          5 },
+	{ A,          5 },
 	{ NOTHING,   20 }, // Aha! Play bells are ringing! I gotta set up the pins, but I'll be back in a flurry
 	{ A,          5 }, // <Continue>
 	{ NOTHING,  325 }, // Cut to different scene (Knock 'em flat!)
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ A,          5 }, // <Continue> // Camera transition takes place after this
-	{ NOTHING,   50 },
-	{ B,          5 },
-	{ NOTHING,   20 }, // If you can knock over all 10 pins in one roll, that's a strike
-	{ A,          5 }, // <Continue>
-	{ NOTHING,   15 },
-	{ B,          5 },
-	{ NOTHING,   20 }, // A spare is...
-	{ A,          5 }, // <Continue>
-	{ NOTHING,  100 }, // Well, good luck
-	{ A,          5 }, // <Continue>
-	{ NOTHING,  150 }, // Pondo walks away
-
-	// Pick up Snowball (Or alternatively, run to bail in case of a non-strike)
-	{ A,          5 },
-	{ NOTHING,   50 },
-	{ LEFT,      42 },
-	{ UP,        80 },
-	{ THROW,     25 },
-
-	// Non-strike alternative flow, cancel bail and rethrow
-	{ NOTHING,   30 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 }, // I have to split dialogue (It's nothing)
-	{ NOTHING,   15 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,  450 },
-	{ B,          5 }, // Snowly moly... there are rules!
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 }, // Second dialogue
-	{ NOTHING,   20 },
-	{ DOWN,      10 }, // Return to snowball
-	{ NOTHING,   20 },
-	{ A,          5 }, // Pick up snowball, we just aimlessly throw it
-	{ NOTHING,   50 },
-	{ UP,        10 },
-	{ THROW,     25 },
-
-	// Back at main flow
-	{ NOTHING,  175 }, // Ater throw wait
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 }, // To the rewards
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
 	
-	{ B,          5 }, // Wait for 450 cycles by bashing B (Like real players do!)
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 },
-	{ B,          5 },
-	{ NOTHING,   20 } // Saving, intermission
+	// First Match
+	{ B,          5 }, // Welcome, bla bla bla
+	{ A,          5 }, // Let's get right to first round
+	{ NOTHING,   50 },
+	{ B,          5 }, // champion ... will be facing
+	{ NOTHING,   20 }, 
+	{ A,          5 }, // text depending on opponent
+	{ NOTHING,   15 },
+	{ B,          5 }, // opponent dialogue
+	{ NOTHING,  100 }, 
+	{ A,          5 }, // you are challenged by ..
+	{ NOTHING,   30 }, // Well, good luck
+	{ A,          5 }, // First pokemon, zoroark
+	{ NOTHING,   20 }, 
+	{ A,          5 }, // taps battle
+	{ NOTHING,   15 }, // 
+	{ A,          5 }, // taps happy hour
+	{ NOTHING,  100 }, // 
+	{ A,          5 }, // taps battle
+	{ NOTHING,   15 }, // 
+	{ DOWN,       5 }, // taps down
+	{ NOTHING,   15 }, // 
+	{ A,          5 }, // taps memento
+	{ NOTHING,  150 }, //
+	{ DOWN,       5 }, // taps down for sylveon
+	{ NOTHING,   15 }, // 
+	{ A,          5 }, // taps to choose
+	{ NOTHING,   15 }, //
+	{ A,          5 }, // YES
+	{ NOTHING,   50 }, //
+	{ A,          5 }, // taps battle
+	{ NOTHING,   15 }, // 
+	{ R,          5 }, // TERASTALLIZE!
+	{ NOTHING,   15 }, //
+	{ A,          5 }, // Hyper Voice
+	{ NOTHING,  250 }, // first pokemon should KO in one hit
+	{ B,          5 }, // skip swap
+	{ B,          5 }, // press B multiple times in case level up and learn new skills
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ A,          5 }, // taps battle
+	{ NOTHING,   15 }, //
+	{ A,          5 }, // Hyper Voice
+	{ NOTHING,  150 }, // second mon
+	{ B,          5 }, // skip swap
+	{ B,          5 }, // press B multiple times in case level up and learn new skills
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ A,          5 }, // taps battle
+	{ NOTHING,   15 }, //
+	{ A,          5 }, // Hyper Voice
+	{ NOTHING,  150 }, // third mon
+	{ B,          5 }, // skip swap
+	{ B,          5 }, // press B multiple times in case level up and learn new skills
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ A,          5 }, // taps battle
+	{ NOTHING,   15 }, //
+	{ A,          5 }, // Hyper Voice
+	{ NOTHING,  150 }, // 4 mon
+	{ B,          5 }, // skip swap
+	{ B,          5 }, // press B multiple times in case level up and learn new skills
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ A,          5 }, // taps battle
+	{ NOTHING,   15 }, //
+	{ A,          5 }, // Hyper Voice
+	{ NOTHING,  150 }, // 5 mon
+	{ B,          5 }, // skip swap
+	{ B,          5 }, // press B multiple times in case level up and learn new skills
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ A,          5 }, // taps battle
+	{ NOTHING,   15 }, //
+	{ A,          5 }, // Hyper Voice
+	{ NOTHING,  300 }, // 6 mon (tera animation is so long)
+	{ B,          5 }, //end of first round battle
+	{ B,          5 }, // press B multiple times in case level up and learn new skills
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ NOTHING,   50 }, // wait for text
+	{ B,          5 }, // and the winner for ...
+	{ B,          5 }, // press B multiple times in case level up and learn new skills
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+	{ B,          5 }, // 
+
 };
 
 // Main entry point.
@@ -439,6 +423,9 @@ void GetNextReport(USB_JoystickReport_Input_t* const ReportData) {
 				case TRIGGERS:
 					ReportData->Button |= SWITCH_L | SWITCH_R;
 					break;
+
+				case CAPTURE:
+					ReportData->Button |= SWITCH_CAPTURE;
 
 				default:
 					ReportData->LX = STICK_CENTER;
